14b6e2728fce9de8521d7ab3278da853de426de2
Alex Scriven <ascriven@keihin-na.com>
2020-04-11 18:57:38 -0400

    Made simple menu, add simple objects, do simple input events

---
+++ PhaserDemo/GameAssets/Javascript/Main.js
@@ -0,0 +1,59 @@
+﻿var config = {
+    type: Phaser.AUTO,
+    width: 1152,
+    height: 648,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 }
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
+
+class GameState {
+    constructor() {
+        this.state = null;
+        this.menu = null;
+        this.map = null;
+    }
+}
+var gameState = new GameState();
+var game = new Phaser.Game(config);
+
+function preload() {
+    this.load.image('MapMap', 'GameAssets/Images/Map/Map.png');
+    this.load.image('MenuMenu', 'GameAssets/Images/Menu/Menu.png');
+    this.load.image('BuildingCity', 'GameAssets/Images/Building/City.png');
+    this.load.image('BuildingFarm', 'GameAssets/Images/Building/Farm.png');
+}
+
+function create() {
+    gameState.map = this.add.sprite(576, 324, 'MapMap').setInteractive();
+    gameState.state = this;
+    CreateMenu();
+}
+function CreateMenu() {
+    gameState.state.physics.add.image(50, 120, 'MenuMenu');
+    var menuBuildingCity = gameState.state.physics.add.image(50, 50, 'BuildingCity').setInteractive();
+    menuBuildingCity.on('pointerup', function (pointer) {
+        gameState.map.on('pointerup', function (pointer) {
+            PlaceBuilding(pointer, "BuildingCity");
+        });
+    });
+    //menu.add(menuBuildingCity);
+
+    var menuBuildingFarm = gameState.state.physics.add.image(50, 90, 'BuildingFarm').setInteractive();
+    menuBuildingFarm.on('pointerup', function (pointer) {
+        gameState.map.on('pointerup', function (pointer) {
+            PlaceBuilding(pointer,"BuildingFarm");
+        });
+    });
+    //menu.add(menuBuildingFarm);
+}
+function PlaceBuilding(pointer,BuildingName) {
+    gameState.state.physics.add.image(pointer.upX, pointer.upY, BuildingName);
+}
