6ee79a72e0332670e54745c1eade782020a783f8
JoshS <Scriven.josh@gmail.com>
2020-04-12 12:03:21 -0400

    Update for population growth

--- PhaserDemo/GameAssets/Javascript/Classes/City.js
+++ PhaserDemo/GameAssets/Javascript/Classes/City.js
@@ -4,5 +4,52 @@
         this.population = 0;
         this.graphic = null;
         this.citiesFarm = new Array();
+        this.foodReserve = 0;
+        this.GrowthRate = ""; //Set cities growth rate, changes population growth formula (meager, standard, abundant) Use more food to increase population rate
+    }
+    //Run this to make city population to grow
+    populationGroth() {
+        switch (this.GrowthRate) {
+            case "meager":
+                if (this.foodReserve > .5 * (this.population)) {
+                    this.population = this.population * (0.2);
+                    this.foodReserve -= .5 * (this.population);
+                }
+                else {
+                    this.population = this.population - (.1 * (this.foodReserve - (.5 * this.population)));
+                    this.foodReserve = 0;
+                }
+                break;
+            case "abundant":
+                if (this.foodReserve > (2.5 * this.population)) {
+                    this.population = this.population * (0.10);
+                    this.foodReserve -= 2.5 * (this.population);
+                }
+                else {
+                    this.population = this.population - (.1 * (this.foodReserve - (2.5 * this.population)));
+                    this.foodReserve = 0;
+                }
+                break;
+            case "standard":
+                if (this.foodReserve > (this.population)) {
+                    this.population = this.population * (0.05);
+                    this.foodReserve -= this.population;
+                }
+                else {
+                    this.population = this.population - (.1 * (this.foodReserve - (this.population)));
+                    this.foodReserve = 0;
+                }
+        }
+    }
+    setGrowthRate(selectedGrowthRate) {
+        if (selectedGrowthRate === 2) {
+            this.GrowthRate = "meager";
+        }
+        else if (selectedGrowthRate === 3) {
+            this.GrowthRate = "abundant";
+        }
+        else {
+            this.GrowthRate = "standard";
+        }
     }
 }
